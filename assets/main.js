de7b44324ebf0fac7ebfffc524489ff9460c559c
andrew fritz <andrew.fritz86@gmail.com>
2015-08-12 13:40:37 -0400

    fix pulsing picture

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,8 @@
 console.log("yes");
+
+document.querySelector("img").addEventListener("mouseover", function(e){
+  e.target.setAttribute("class", "animated pulse");
+  window.setTimeout(function(){
+    e.target.setAttribute("class", "")
+  },800)
+})
