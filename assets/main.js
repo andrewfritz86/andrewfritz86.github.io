a8c5fa8ec74ea61ba88f881e05d841917e55aa80
andrewfritz86 <andrew.fritz86@gmail.com>
2016-07-15 21:16:58 -0400

    update

--- assets/main.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelector("img").addEventListener("mouseover", function(e){
-  e.target.setAttribute("class", "animated pulse");
-  window.setTimeout(function(){
-    e.target.setAttribute("class", "")
-  },800)
-})
