33a79767e042e7269beec28454838ee64b836e9f
andrewfritz86 <andrew.fritz86@gmail.com>
2018-01-15 12:46:14 -0500

    2018 updates

--- assets/main.js
+++
@@ -1,9 +0,0 @@
-
-
-var toggleUnderline = function toggleUnderline() {
-  var attempt = document.querySelector('.attempting');
-  attempt.setAttribute('class', 'attempting underline');
-}
-
-window.setTimeout(toggleUnderline, 100);
-
